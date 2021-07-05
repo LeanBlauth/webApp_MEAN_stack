9de5e27c0a8fca95438fc02c0f58ff75fb908095
LeanBlauth <le4ndroblauth@gmail.com>
2021-07-05 19:35:06 -0300

    added api controller endpoint

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ var app = express();
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 var port = process.env.PORT || 3000;
 app.use('/assets', express.static(__dirname+'/public'));
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConectionString());
 setupController(app);
+apiController(app);
 
 app.listen(port);
 
